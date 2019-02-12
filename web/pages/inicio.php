4b8365e3d56c751784687d39d5d888c59414b5a1
coco <coco@lacueva.tv>
2019-02-12 08:23:02 -0300

    first commit

---
+++ web/pages/inicio.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * PAGE TEMPLATE: PAGINA INICIO
+ * es el html de la pagina de inicio
+*/
+getTemplate( 'header' ); ?>
+<div class="inner-wrapper">
+    inicio
+</div><!-- // INNVER WRAPPER -->
+<?php getTemplate( 'footer' );
---
+++ web/templates/footer.php
@@ -0,0 +1,10 @@
+	<footer class="main-footer">
+		Footer
+	</footer>
+	
+</div><!-- // MAIN WRAPPER -->
+	<script src="<?php echo MAINSURL; ?>/assets/js/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo MAINSURL; ?>/assets/js/script.js"></script>
+
+</body>
+</html>
